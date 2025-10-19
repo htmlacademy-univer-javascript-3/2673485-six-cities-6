23eace7389e3f38b16eb7337b03f261ed0491aa2
Manykeys <kopyrkin.artyom@yandex.ru>
2025-10-19 22:58:19 +0500

    Fourth Task. Refactor components

--- src/components/PrivateRoutes/PrivateRoutes.tsx
+++ src/components/PrivateRoutes/PrivateRoutes.tsx
@@ -4,13 +4,14 @@ import React from 'react';
 import Offer from '../../pages/offer/Offer.tsx';
 import Login from '../../pages/login/Login.tsx';
 import Favorites from '../../pages/favorites/Favorites.tsx';
+import { Card } from '../../types/Card.tsx';
 
 
-export function PrivateRoutes({isAuthorized} : { isAuthorized: boolean }){
+export function PrivateRoutes({isAuthorized, offers} : { isAuthorized: boolean; offers: Card[] }) {
   if (isAuthorized){
     return (
       <React.Fragment>
-        <Route path={AppRoute.Favourites} element={<Favorites />} />
+        <Route path={AppRoute.Favourites} element={<Favorites offers={offers} />} />
         <Route path={AppRoute.Offer} element={<Offer />} />
         <Route path={AppRoute.Login} element={<Navigate to={AppRoute.Main} replace />}/>
       </React.Fragment>
